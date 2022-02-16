392b378bd842dcb23242f9b7df4464212d87474d
IsaiahMars <93736007+IsaiahMars@users.noreply.github.com>
2022-02-15 20:24:04 -0500

    UI Structure
    -Created a basic structure for a UI
    -No scripts added yet (will work on
      adding functionality tomorrow)
    
    Co-Authored-By: schen019 <57651847+schen019@users.noreply.github.com>

---
+++ UnityEndlessRunner/Assets/Scripts/ScoreTracker.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ScoreTracker : MonoBehaviour
+{
+    [SerializeField] int score, topScore;
+    public Text scoreText, topScoreText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
