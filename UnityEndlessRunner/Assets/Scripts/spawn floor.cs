22975403102ebfd407df5e39d8c46bd957fdf2c1
Jason-J5 <jacobsjason421@gmail.com>
2022-02-28 12:09:05 -0500

    added prefabs and tags to walls

--- UnityEndlessRunner/Assets/Scripts/spawn floor.cs
+++ UnityEndlessRunner/Assets/Scripts/spawn floor.cs
@@ -7,7 +7,7 @@ public class test : MonoBehaviour
 
     //basic array for storing tiles currently spawned 
     //9 tiles ahead and 1 tile behind 
-    GameObject [10] tiles = GameObject.FindGameObjectsWithTag("Tile");
+    
 
 
 
@@ -15,12 +15,12 @@ public class test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3(10, 0, 5);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3();
+        
     }
 }
