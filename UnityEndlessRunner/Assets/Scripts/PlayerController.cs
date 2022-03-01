51e5944263965ed7d99ae4e4cf9c14bc6b4fd42f
N1Andrew <63069333+N1Andrew@users.noreply.github.com>
2022-03-01 12:00:22 -0500

    PlayerController Fixed

--- UnityEndlessRunner/Assets/Scripts/PlayerController.cs
+++ UnityEndlessRunner/Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-public class PlayerController : MonoBehaviour
+public class RunnerControl : MonoBehaviour
 {
     public bool hit;
     public bool jump;
