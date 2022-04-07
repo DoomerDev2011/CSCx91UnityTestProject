986ef8bf821d380ba164e792a6f7eb0976137aa6
IsaiahMars <93736007+IsaiahMars@users.noreply.github.com>
2022-04-07 19:52:51 -0400

    Environment Gen v0.13
    -Added main menu screen.
    -There is a weird bug that makes the environment gen scene really dark when switched to from the main menu screen.

--- UnityEndlessRunner/Assets/Scripts/MainMenuHandler.cs
+++ UnityEndlessRunner/Assets/Scripts/MainMenuHandler.cs
@@ -2,22 +2,84 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MainMenuHandler : MonoBehaviour
 {
-    public GameObject mainMenu;
     public Button playButton, settingsButton, quitButton;
-
+    public GameObject settingsMenu;
+    public Canvas uiCanvas;
+    public Slider soundSlider;
+    public Button returnButton;
+    public Dropdown resolutionDropdown;
+    public Toggle fullscreenToggle;
+    
     private bool isActive = false;
+    private bool isFullscreen = true;
 
-    void Start()
-    {
-        mainMenu.SetActive(isActive);
+    void Awake(){
+        settingsMenu.SetActive(isActive);
+        initPlayerSettings();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.Escape) && isActive){
+            Return();
+        }
     }
+
+    public void Quit(){
+        Application.Quit();
+    }
+
+    public void openSettings(){
+       isActive = true;
+       settingsMenu.SetActive(isActive);
+    }
+
+    public void Play(){
+        SceneManager.LoadScene("EnvironmentGen");
+    }
+
+    public void Return(){
+       isActive = false;
+       settingsMenu.SetActive(isActive); 
+       PlayerPrefs.Save();
+    }
+
+    public void soundScale(){
+        AudioListener.volume = soundSlider.value;
+        PlayerPrefs.SetFloat("soundScale", soundSlider.value);
+    }
+
+    public void toggleFullscreen(){
+        isFullscreen = !isFullscreen;
+        PlayerPrefs.SetInt("isFullscreen", isFullscreen ? 1 : 0);
+        resolutionChanger();
+    }
+
+    public void resolutionChanger(){
+        switch(resolutionDropdown.value){
+            case 0:
+                Screen.SetResolution(1920,1080, isFullscreen);
+                break;
+            case 1:
+                Screen.SetResolution(1280,1024, isFullscreen);
+                break;
+            case 2:
+                Screen.SetResolution(800,600, isFullscreen);
+                break;
+        }
+        PlayerPrefs.SetInt("resDropdownValue", resolutionDropdown.value);
+    }
+
+    public void initPlayerSettings(){
+        soundSlider.value = PlayerPrefs.GetFloat("soundScale");
+        soundScale();
+        resolutionDropdown.value = PlayerPrefs.GetInt("resDropdownValue");
+        isFullscreen = PlayerPrefs.GetInt("isFullscreen")==1 ? true : false;
+        fullscreenToggle.SetIsOnWithoutNotify(isFullscreen);
+        resolutionChanger();
+    }
+
 }
