defb06ec47f39dfc577b2865b2f5ecc67ed5af47
Jason Jacobs <jjaco019@cslab-167.cs.plattsburgh.edu>
2022-04-14 11:28:50 -0400

    Testing time slow down

--- UnityEndlessRunner/Assets/Scripts/TimeSlowDown.cs
+++ UnityEndlessRunner/Assets/Scripts/TimeSlowDown.cs
@@ -8,7 +8,17 @@ public class TimeSlowDown : MonoBehaviour {
     public float timer = 60;
     public bool active = false;
 
-    void OnTriggerEnter(Collider other) {
+
+    private float fixedDeltaTime;
+
+    void Awake()
+    {
+        // Make a copy of the fixedDeltaTime, it defaults to 0.02f, but it can be changed in the editor
+        this.fixedDeltaTime = Time.fixedDeltaTime;
+    }
+
+
+    void OnCollisionEnter(Collision other){
         if (other.gameObject.tag == "Player"){
             active = true;
             Time.timeScale = 0.5f;
@@ -17,13 +27,16 @@ public class TimeSlowDown : MonoBehaviour {
     }
 
     private void Update(){
-        while (active){
-            timer -= Time.deltaTime;
+        while(active){
+            timer -= Time.fixedDeltaTime;
             Debug.Log("Timer: " + timer);
             if(timer == 0){
                 active = false;
                 Time.timeScale = 1f;
                 timer = 60;
+                // Adjust fixed delta time according to timescale
+                // The fixed delta time will now be 0.02 real-time seconds per frame
+                Time.fixedDeltaTime = this.fixedDeltaTime * Time.timeScale;
             }
         }
     }
