52ac6d767faac42873c3d6ac2d878524578552e7
schen019 <schen019@plattsburgh.edu>
2022-05-03 11:28:15 -0400

    new update

--- UnityEndlessRunner/Assets/Scripts/GameManager.cs
+++ UnityEndlessRunner/Assets/Scripts/GameManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
-    public float timer = 60;
+    public float timer = 20;
     public float Coins = 0;
 
 }
--- UnityEndlessRunner/Assets/Scripts/TimeSlowDown.cs
+++ UnityEndlessRunner/Assets/Scripts/TimeSlowDown.cs
@@ -5,25 +5,30 @@ using UnityEngine;
 
 public class TimeSlowDown : MonoBehaviour {
 
-    public float timer = 20;
     public bool active = false;
+    private GameManager gm;
+
+    void Start(){
+        gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
 
     void OnTriggerEnter(Collider other){
         if (other.gameObject.tag == "Player"){
             active = true;
             Time.timeScale = 0.5f;
+            Destroy(this.gameObject);
         }
     }
 
     void Update(){
         if (active){
-            if(Mathf.Ceil(timer) > 0){
-                timer -= Time.deltaTime * 2;
-                Debug.Log("Timer: " + Mathf.Ceil(timer));
+            if(Mathf.Ceil(gm.timer) > 0){
+                gm.timer -= Time.deltaTime * 2;
+                Debug.Log("Timer: " + Mathf.Ceil(gm.timer));
             }else{
                 active = false;
                 Time.timeScale = 1f;
-                timer = 20;
+                gm.timer = 20;
             }
         }
     }
