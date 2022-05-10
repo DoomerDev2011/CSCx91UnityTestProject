a4ec9a7599188e3ee3f0bb891c69ab21e80a77fa
schen019 <schen019@plattsburgh.edu>
2022-05-10 12:10:35 -0400

    add some comments

--- UnityEndlessRunner/Assets/Scripts/GameManager.cs
+++ UnityEndlessRunner/Assets/Scripts/GameManager.cs
@@ -3,23 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
-{
+{  
+    /*make some public variables to keep the ability scripts*/
     public static GameManager instance;
+    /*Coin script*/
     public float Coins = 0;
+    /*TimeSlowDown script*/
     public float slowtimer = 10;
-    public bool active = false;
+    public bool slowing = false;
+    /*SpeedBoost script*/
     public float speed;
     public float boosttimer = 10;
     public bool boosting = false;
 
 
 
-
+    /*make the updata during the game*/
     void Update(){
+        /*when the boosting is true*/
         if (boosting){
+            /*if the variable boosttimer greater then 0*/
             if(Mathf.Ceil(boosttimer)>0){
-                if(active){
-                    boosttimer -= Time.deltaTime*2;
+                /*if the variable slowing also true*/
+                if(slowing){
+                    boosttimer -= Time.deltaTime*2;/*the variable boosttimer*/
                 }else{
                     boosttimer -= Time.deltaTime;
                 }
@@ -30,12 +37,12 @@ public class GameManager : MonoBehaviour
                 boosttimer = 10;
             }
         }
-        if (active){
+        if (slowing){
             if(Mathf.Ceil(slowtimer) > 0){
                 slowtimer -= Time.deltaTime * 2;
                 Debug.Log("Timer: " + Mathf.Ceil(slowtimer));
             }else{
-                active = false;
+                slowing = false;
                 Time.timeScale = 1f;
                 slowtimer = 10;
             }
--- UnityEndlessRunner/Assets/Scripts/TimeSlowDown.cs
+++ UnityEndlessRunner/Assets/Scripts/TimeSlowDown.cs
@@ -2,21 +2,22 @@ using System.Collections;
 
 using System.Collections.Generic;
 using UnityEngine;
-
+/*Here is the script to have the Time Slow Down ability*/
 public class TimeSlowDown : MonoBehaviour {
-
+    /*add the variable 'gm' in this script*/
     private GameManager gm;
-
+        /*defind and set GameManager to the GameObject 'GameManager' when the program start*/
     void Start(){
         gm = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
-
+    /*When the 'player' hit this Object'*/
      void OnCollisionEnter(Collision other){
          if (other.gameObject.tag == "Player"){
-            gm.active = true;
-            Time.timeScale = 0.5f;
-            Destroy(this.gameObject);
+            gm.slowing = true;/*the variable'active' in gm will set to true*/
+            gm.slowtimer = 10;
+            Time.timeScale = 0.5f;/*Set the game timespeed to 0.5 */
+            Destroy(this.gameObject);/*Delete the this gameObjrct*/
         }
     }
 
